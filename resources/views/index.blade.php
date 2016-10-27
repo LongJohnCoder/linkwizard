8904894812cd2f1d690572287e4f70129ece262f
Tier5djm <work@tier5.us>
2016-10-27 13:09:55 +0530

    Fixed hello@tier5.us mailto link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -148,7 +148,7 @@
                         <div id="why" class="tab-pane fade">
                             <p>By registering to our site you can track your list of shortend urls. Also you could monitor no of clicks, hits, and statistics of each and every single urls. Rest assured your information will not be sold, traded, or given to anyone.</p>
                             <p> Please contact
-                                <a mailto:href="hello@tier5.us"></a>hello@tier5.us for any other inquiries.</p>
+                                <a href="mailto:hello@tier5.us">hello@tier5.us</a> for any other inquiries.</p>
                         </div>
                         <div id="signin" class="tab-pane fade active in">
                             <form method="post" action="{{ route('postLogin') }}">
