d971a99a4dab700586a380e07f347cc3a5301361
bhaskar <github@tier5.us>
2018-03-09 18:05:41 +0530

    add search bar for descriptions and tags v1.0

---
+++ resources/views/contents/footer.blade.php
@@ -0,0 +1,28 @@
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="footer-menu">
+					<ul>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Features</a></li>
+						<li><a href="#">Blog</a></li>
+						<li><a href="#">FAQ</a></li>
+						<li><a href="#">Terms</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">Partners</a></li>
+						<li><a href="#">Privacy</a></li>
+					</ul>
+				</div>
+				<div class="social-icon">
+					<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+					<a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+				</div>
+				<div class="copyright">
+					<p>© All Rights Reserved to Tier5 LLC. </p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
--- resources/views/dashboard2.blade.php
+++ resources/views/dashboard2.blade.php
@@ -1,287 +1,16 @@
 <!DOCTYPE html>
+
+<!-- head of th page -->
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Dashboard</title>
-
-<link href="{{url('/')}}/public/css/bootstrap.min.css" rel="stylesheet">
-<link href="{{url('/')}}/public/css/style.css" rel="stylesheet">
-<link rel="stylesheet" href="{{url('/')}}/public/fonts/font-awesome/css/font-awesome.min.css">
-
-<meta name="description" content="An URL shortener with more sophisticated analytics. Spread your business or creativity using the power of shorten links. Brought to you by Tier5 LLC." />
-<meta name="keywords" content="Tier5 URL Shortner, Tr5.io, Tier5" />
-<meta name="author" content="Tier5 LLC" />
-<link rel="stylesheet" href="{{ URL('/')}}/public/resources/css/bootstrap.min.css" />
-<link rel="stylesheet" href="{{ URL('/')}}/public/resources/css/bootstrap-theme.min.css" />
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" />
-<link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Nunito:400,300,700' />
-<link rel="stylesheet" href="{{ URL('/')}}/public/resources/css/style2.css" />
-
-<link rel="stylesheet" href="{{ URL('/')}}/public/resources/css/custom.css" />
-<link rel="stylesheet" href="https://sdkcarlos.github.io/sites/holdon-resources/css/HoldOn.css" />
-<link rel="stylesheet" href="{{ URL('/')}}/public/resources/css/bootstrap-datepicker3.standalone.min.css" />
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<script src="{{ URL::to('/').'/public/resources/js/bootstrap.min.js'}}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.5.12/clipboard.min.js"></script>
-<script src="https://www.gstatic.com/charts/loader.js"></script>
-<script src="https://www.google.com/jsapi"></script>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.0-rc.2/Chart.bundle.min.js"></script> --}}
-<script src="{{ URL::to('/').'/public/resources/js/highcharts.js' }}"></script>
-<script src="{{ URL::to('/').'/public/resources/js/highchart-data.js' }}"></script>
-<script src="{{ URL::to('/').'/public/resources/js/highchart-drilldown.js' }}"></script>
-<script src="{{ URL::to('/').'/public/resources/js/modernizr.custom.js' }}"></script>
-
-
-{{-- Tags css --}}
-<link rel="stylesheet" href="{{ URL('/')}}/public/css/bootstrap-tagsinput.css" />
-
-{{-- Tags js --}}
-<script src="{{URL::to('/').'/public/js/bootstrap-tagsinput.js'}}"></script>
-
-@if(\Session::has('plan'))
-<script type="text/javascript">
-	$(document).ready(function(){
-		var plan = "{{\Session::get('plan')}}";
-		var url = "{{url('/')}}/app/user/subscribe";
-		window.location.replace(url);
-	});
-</script>
-@endif
-
-
-
-
-<link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
-
-<!-- <script src="{{url('/')}}/public/js/bootstrap.min.js"></script> -->
-
-
-
-
-
-<meta property="og:title" content="Tier5 URL Shortener" />
-<meta property="og:image" content="{{ URL('/')}}/public/resources/img/company_logo.png" />
-<meta property="og:url" content="tr5.io" />
-<meta property="og:site_name" content="Tr5.io" />
-<meta property="og:description" content="An URL shortener with more sophisticated analytics. Spread your business or creativity using the power of shorten links. Brought to you by Tier5 LLC." />
-<meta name="twitter:title" content="Tr5.io"  />
-<meta name="twitter:image" content="{{ URL('/')}}/public/resources/img/company_logo.png"  />
-<meta name="twitter:url" content="tr5.io"  />
-<meta name="twitter:card" content="summary"  />
-
-
-<script src="//connect.facebook.net/en_US/sdk/debug.js"></script>
-<script src="{{ URL::to('/').'/public/js/fb_share.js'}}"></script>
-
-<!-- /Facebook API -->
-<!-- Google API -->
-<script>
-    window.___gcfg = {
-        lang: 'en-US',
-        parsetags: 'onload'
-    };
-</script>
-<script src="https://apis.google.com/js/client:platform.js" async defer></script>
-<!-- /Google API -->
-<!-- Twitter API -->
-<script>
-    window.twttr = (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-        t = window.twttr || {};
-        if (d.getElementById(id)) return t;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://platform.twitter.com/widgets.js";
-        fjs.parentNode.insertBefore(js, fjs);
-
-        t._e = [];
-        t.ready = function(f) {
-            t._e.push(f);
-        };
-
-        return t;
-    }(document, "script", "twitter-wjs"));
-</script>
-<script type="text/javascript" async src="https://platform.twitter.com/widgets.js"></script>
-<!-- /Twitter API -->
-<!-- LinkedIn API -->
-<script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script>
-<!-- /LinkedIn API -->
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.2/summernote.css" />
-
-</head>
+	@include('contents/head')
 <body>
+<!-- head end -->
+
 <!-- Header Start -->
-
-
-
-<header>
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-md-4">
-				<div class="logo">
-					 <a href="{{url('/')}}/about"><img src="{{url('/')}}/public/images/logo.png" class="img-responsive"></a>
-				</div>
-			</div>
-
-			<div class="col-md-8">
-				<div class="top-right">
-					@if(count($limit) > 0)
-					<div class="createlink openpop">
-						<a href="javascript:void(0)" id="basic">Create tier5 link</a>
-					</div>
-					@endif
-					@if ($subscription_status != null)
-						 @if(count($limit) > 0)
-						 	<div class="createlink openpop">
-						 		<a href="javascript:void(0)" id="advanced" style="background-color:red">Create Custom link</a>
-						 	</div>
-						 @endif
-					@endif
-					<div class="hamburg-menu">
-	                  <a href="#" id="menu-icon" class="menu-icon">
-	                    <div class="span bar top" style="background-color: #fff;"></div>
-	                    <div class="span bar middle" style="background-color: #fff;"></div>
-	                    <div class="span bar bottom" style="background-color: #fff;"></div>
-	                  </a>
-	                </div>
-	                <div id="userdetails" class="userdetails">
-	                	<div>
-		                	<a href="{{ route('getLogout') }}" class="signout"><i class="fa fa-sign-out"></i> Sign out</a>
-		                	<p style="color:white">{{ $user->name }}</p>
-		                	<p style="color:white">{{ $user->email }}</p>
-		                	@if ($subscription_status != 'tr5Advanced')
-		                		<a href="{{ route('getSubscribe') }}" class="upgrade"><i class="fa fa-sign-out"></i> Upgrade</a>
-		                	@endif
-	                	</div>
-	                </div>
-
-	                <div id="myNav1" class="userdetails">
-	                	<!-- <a href="#" id="cross1" class="closebtn"><i class="fa fa-times" style="color:white"></i></a> -->
-                        <div class="overlay-content">
-	                        <div class="col-md-12 col-sm-12">
-	                            <label for="givenUrl" style="color:white">Paste An Actual URL Here</label>
-	                            <input id="givenUrl" class="myInput form-control" type="text" name="" placeholder="Paste Your URL Here">
-															<br>
-															<label for="addFbPixelid" style="color:white">Add facebook pixel</label>
-															<input id="checkboxAddFbPixelid" type="checkbox" name="chk_fb_short" style="color: white">
-															<input id="fbPixelid" class="myInput form-control" type="number" name="client_fb_pixel_id" placeholder="Paste Your Facebook-pixel-id Here" style="display : none">
-
-															<br>
-															<label for="addGlPixelid" style="color:white">Add google pixel</label>
-															<input id="checkboxAddGlPixelid" type="checkbox" name="chk_gl_short" style="color: white">
-															<input id="glPixelid" class="myInput form-control" type="number" name="client_gl_pixel_id" placeholder="Paste Your Google-pixel-id Here" style="display : none">
-
-															<br>
-															<label for="shortTags" style="color:white">Add tags</label>
-															<input id="shortTagsEnable" type="checkbox" name="shortTagsEnable" style="color: white">
-															<div id="shortTagsArea" style="display: none">
-																<input id="shortTagsContents" class="myInput form-control" data-role="tagsinput" type="text" name="shortTagsContents" placeholder="Mention tags for this link" style="display: none">
-															</div>
-
-															<br>
-																<label for="shortDescription" style="color:white">Add description</label>
-																<input id="shortDescriptionEnable" type="checkbox" name="shortDescriptionEnable" style="color: white">
-																<input id="shortDescriptionContents" class="myInput form-control description" type="textarea" name="shortDescriptionContents" placeholder="Mention description for this link" style="display : none">
-
-																<button id="swalbtn" type="submit" class="btn btn-primary btn-sm">
-		                                Shorten Url
-		                            </button>
-															<br>
-	                        </div>
-		                </div>
-	                </div>
-
-	                <div id="myNav2" class="userdetails">
-		                <!-- <a href="#" id="cross2" class="closebtn"><i class="fa fa-times" style="color:white"></i></a> -->
-		                <div class="overlay-content">
-	                        <div class="col-md-12 col-sm-12">
-	                            <label for="givenActualUrl" style="color:white;">Paste An Actual URL Here</label>
-	                            <input id="givenActualUrl" style="width:280px" class="myInput form-control" type="text" name="" placeholder="Paste Your URL Here">
-	                            <br>
-	                            <br>
-	                            <label for="makeCustomUrl" style="color:white">Create Your Own Custom Link</label>
-	                            <div class="input-group">
-	                                <span class="input-group-addon">{{ env('APP_HOST') }}</span>
-	                                <input id="makeCustomUrl" class="myInput form-control" type="text" name="" placeholder="e.g. MyLinK">
-	                            </div>
-															<br>
-
-															<label for="checkboxAddFbPixelid1" style="color:white">Add facebook pixel</label>
-															<input id="checkboxAddFbPixelid1" type="checkbox" name="chk_fb_custom" style="color: white">
-															<input id="fbPixelid1" class="myInput form-control" type="number" placeholder="Paste Your Facebook-pixel-id Here" style="display : none">
-															<br>
-															<label for="addGlPixelid1" style="color:white">Add google pixel</label>
-															<input id="checkboxAddGlPixelid1" type="checkbox" name="chk_gl_custom" style="color: white">
-															<input id="glPixelid1" class="myInput form-control" type="number" name="client_gl_pixel_id1" placeholder="Paste Your Google-pixel-id Here" style="display : none">
-
-															<br>
-															<label for="customTags" style="color:white">Add tags</label>
-															<input id="customTagsEnable" type="checkbox" name="customTagsEnable" style="color: white">
-															<div id="customTagsArea" style="display: none">
-																<input id="customTagsContents" class="myInput form-control" data-role="tagsinput" type="text" name="customTagsContents" placeholder="Mention tags for this link" style="display: none">
-															</div>
-
-															<br>
-																<label for="customDescription" style="color:white">Add description</label>
-																<input id="customDescriptionEnable" type="checkbox" name="customDescriptionEnable" style="color: white">
-																<input id="customDescriptionContents" class="myInput form-control description" type="textarea" name="customDescriptionContents" placeholder="Mention description for this link" style="display : none">
-
-	                            <button id="swalbtn1" type="submit" class="btn btn-primary btn-sm">
-	                                Shorten Url
-	                            </button>
-	                            <br>
-	                            <span id="err_cust" style="color:red; display:none;" >This URL is taken. Please try with a different name</span>
-	                        </div>
-		                </div>
-		            </div>
-
-
-		            <div class="top-menu dashboard-menu">
-						<!-- <div class="mobile-menu">
-							<div class="hamburg-menu">
-				              <a href="#" class="menu-icon" style="display: block;">
-				                <div class="span bar top" style="background-color: #fff;"></div>
-				                <div class="span bar middle" style="background-color: #fff;"></div>
-				                <div class="span bar bottom" style="background-color: #fff;"></div>
-				              </a>
-				            </div>
-				            <ul>
-				            	<li><a href="/about">about</a></li>
-				            	<li><a href="/features">features</a></li>
-				            	<li><a href="/pricing">pricing</a></li>
-				            	<li><a href="/blog">blog</a></li>
-				            	@if ($user->is_admin == 1)
-		               				<li><a style="color:green" href="{{ route('getAdminDashboard') }}">ADMIN DASHBOARD</a></li>
-                    			@endif
-				            </ul>
-				        </div> -->
-				        <div class="desktop-menu">
-				            <ul>
-				            	<li><a href="/about">about</a></li>
-				            	<li><a href="/features">features</a></li>
-				            	<li><a href="/pricing">pricing</a></li>
-				            	<li><a href="/blog">blog</a></li>
-				            	@if ($user->is_admin == 1)
-		               				<li><a style="color:green" href="{{ route('getAdminDashboard') }}">ADMIN DASHBOARD</a></li>
-                    			@endif
-
-				            </ul>
-				        </div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</header>
+@include('contents/header')
 <!-- Header End -->
+
+
 <!-- Banner Start -->
 <div class="modal fade" id="datePickerModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
@@ -316,7 +45,44 @@
     </div>
 </div>
 
+
+<!-- search div -->
+<div class="search-wrap">
+	<div class="container">
+			<div class="row">
+					<div class="col-md-12 col-sm-12">
+							<div class="searchpart-area">
+									<div class="row">
+											<div class="col-md-5 col-sm-5 less-pad">
+												<div class="form-group">
+														<input type="text" placeholder="Search,links" class="form-control">
+												</div>
+											</div>
+											<div class="col-md-5 col-sm-5 less-pad">
+												<div class="form-group">
+													<div>
+														<input type="text" data-role="tagsinput" placeholder="Search tags" class="form-control">
+													</div>
+												</div>
+											</div>
+											<div class="col-md-2 col-sm-2 less-pad">
+												<div class="form-group">
+														<div class="form-group">
+															<button class="btn search-btn">Search</button>
+														</div>
+												</div>
+											</div>
+
+									</div>
+							</div>
+					</div>
+			</div>
+	</div>
+</div>
+<!-- search div ends -->
+
 <section class="banner">
+	{{-- populating this container with google api --}}
 	<div class="container">
 		<div class="row">
 			<div class="banner-top">
@@ -857,34 +623,7 @@
 
 </section>
 
-<footer>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="footer-menu">
-					<ul>
-						<li><a href="#">About</a></li>
-						<li><a href="#">Features</a></li>
-						<li><a href="#">Blog</a></li>
-						<li><a href="#">FAQ</a></li>
-						<li><a href="#">Terms</a></li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="#">Partners</a></li>
-						<li><a href="#">Privacy</a></li>
-					</ul>
-				</div>
-				<div class="social-icon">
-					<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-					<a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-					<a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
-				</div>
-				<div class="copyright">
-					<p>© All Rights Reserved to Tier5 LLC. </p>
-				</div>
-			</div>
-		</div>
-	</div>
-</footer>
+@include('contents/footer')
 
 <script type="text/javascript">
 
