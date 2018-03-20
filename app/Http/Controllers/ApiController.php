b23f2578e26fc82f1facaeb5957846c84fb374a6
Tier5 LLC <github@tier5.us>
2018-03-20 15:22:47 +0530

    WebHook Create User By Email v1.2 #stable

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,114 +10,125 @@ use App\Subscription;
 
 class ApiController extends Controller
 {
-    //
-    public function createUserByEmail(Request $request) {
-      $token = $request->token;
-      $email = $request->email;
-      try {
-        $v = \Validator::make($request->all(), [
-            'email' => 'required|email|unique:users',
-        ]);
-        if($v->fails()) {
-          return \Response::json([
-            "http_code" => 400,
-            "status"    => "error",
-            "message"   => "email address format is incorrect or is already present!"
-          ],400);
-        }
+  /*
+  * Webhook call for create a user with highest membership
+  * Request token, email
+  * Response json
+  */
+  public function createUserByEmail(Request $request)
+  {
+    $token = $request->token;
+    $email = $request->email;
+    try {
+      $v = \Validator::make($request->all(), [
+          'email' => 'required|email|unique:users',
+      ]);
+      if($v->fails()) {
+        return \Response::json([
+          "http_code" => 400,
+          "status"    => "error",
+          "message"   => "email address format is incorrect or is already present!"
+        ],400);
+      }
 
-        if ($token != config('api.token')) {
-          return \Response::json([
-            "http_code" => 400,
-            "status"    => "error",
-            "message"   => "Authentication token incorrect"
-          ],400);
+      if ($token != config('api.token')) {
+        return \Response::json([
+          "http_code" => 400,
+          "status"    => "error",
+          "message"   => "Authentication token incorrect"
+        ],400);
+      } else {
+
+        $name   = explode('@',$email);
+        $name   = $name[0];
+
+        $user                 = new User();
+        $user->name           = $name;
+        $user->email          = $email;
+        $user->password       = bcrypt(config('api.default_password'));
+        $user->remember_token = '';
+
+        if($user->save()) {
+
+            $sb = new Subscription();
+            $sb->user_id      = $user->id;
+            $sb->name         = config('api.subscription.name');
+            $sb->stripe_id    = '';
+            $sb->stripe_plan  = 'tr5Advanced';
+            $sb->quantity     = 1;
+            $sb->save();
+
+            return \Response::json([
+              "http_code" => 200,
+              "status"    => "success",
+              "message"   => "User created successfully with default password!"
+            ],200);
+
         } else {
+          return \Response::json([
+            "http_code" => 500,
+            "status"    => "error",
+            "message"   => "Database connectivity error.. Please try after sometime!"
+          ],500);
+        }
+      }
+    } catch (Exception $e) {
+      return \Response::json([
+        "http_code" => 500,
+        "status"    => "error",
+        "message"   => $e->getMessage()
+      ],500);
+    }
+  }
 
-          $name   = explode('@',$email);
-          $name   = $name[0];
-
-          $user                 = new User();
-          $user->name           = $name;
-          $user->email          = $email;
-          $user->password       = bcrypt(config('api.default_password'));
-          $user->remember_token = '';
-
-          if($user->save()) {
-
-              $sb = new Subscription();
-              $sb->user_id      = $user->id;
-              $sb->name         = config('api.subscription.name');
-              $sb->stripe_id    = '';
-              $sb->stripe_plan  = 'tr5Advanced';
-              $sb->quantity     = 1;
-              $sb->save();
-
-              return \Response::json([
-                "http_code" => 200,
-                "status"    => "success",
-                "message"   => "User created successfully with default password!"
-              ],200);
-
+  /*
+  * Webhook call for delete a user
+  * Request token, email
+  * Response json
+  */
+  public function deleteUserByEmail(Request $request)
+  {
+    $token = $request->token;
+    $email = $request->email;
+    try {
+      if ($token != config('api.token')) {
+        return \Response::json([
+          "http_code" => 400,
+          "status"    => "error",
+          "message"   => "Authentication token incorrect"
+        ],400);
+      } else {
+        $user = User::where('email', $email)->first();
+        if ($user) {
+          if ($user->delete()) {
+            return \Response::json([
+              "http_code" => 200,
+              "status"    => "error",
+              "message"   => "User delete successfull"
+            ],200);
           } else {
             return \Response::json([
               "http_code" => 500,
               "status"    => "error",
-              "message"   => "Database connectivity error.. Please try after sometime!"
+              "message"   => "Database error"
             ],500);
           }
-        }
-      } catch (Exception $e) {
-        return \Response::json([
-          "http_code" => 500,
-          "status"    => "error",
-          "message"   => $e->getMessage()
-        ],500);
-      }
-    }
-
-    public function deleteUserByEmail(Request $request)
-    {
-      $token = $request->token;
-      $email = $request->email;
-      try {
-        if ($token != config('api.token')) {
+        } else {
           return \Response::json([
             "http_code" => 400,
             "status"    => "error",
-            "message"   => "Authentication token incorrect"
+            "message"   => "User not found"
           ],400);
-        } else {
-          $user = User::where('email', $email)->first();
-          if ($user) {
-            if ($user->delete()) {
-              return \Response::json([
-                "http_code" => 200,
-                "status"    => "error",
-                "message"   => "User delete successfull"
-              ],200);
-            } else {
-              return \Response::json([
-                "http_code" => 500,
-                "status"    => "error",
-                "message"   => "Database error"
-              ],500);
-            }
-          } else {
-            return \Response::json([
-              "http_code" => 400,
-              "status"    => "error",
-              "message"   => "User not found"
-            ],400);
-          }
         }
-
-      } catch (Exception $e) {
-        return \Response::json([
-          "http_code" => 500,
-          "status"    => "error",
-          "message"   => $e->getMessage()
-        ],500);
       }
+
+    } catch (Exception $e) {
+      return \Response::json([
+        "http_code" => 500,
+        "status"    => "error",
+        "message"   => $e->getMessage()
+      ],500);
     }
+  }
+
 }
